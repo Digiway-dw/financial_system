7bebadfc79b13bba586325f62ce84eaf17a7c4b2
Mohamed Emad <zemax.c4@gmail.com>
2025-07-17 22:18:09 +0300

    Implement multiple code changes to enhance functionality and improve performance

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,9 +14,8 @@
 
     <!-- Scripts -->
     <!-- Debug: Host is {{ request()->getHost() }} -->
-    <!-- Temporarily forcing built assets -->
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BizWfs3r.css') }}">
-    <script type="module" src="{{ asset('build/assets/app-DsYpAsBD.js') }}"></script>
+    <!-- Using dynamic asset loading -->
+    @viteBuilt(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">
