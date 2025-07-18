b93fc7d0167799a11c9eab84ce4040ccc70c2158
Mohamed Emad <zemax.c4@gmail.com>
2025-07-18 14:31:27 +0300

    fix: Update asset file reference in manifest and remove duplicate vite directive from withdrawal view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <head>
     <title>Fido Dido</title>
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 <body>
